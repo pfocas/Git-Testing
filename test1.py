1aab0117f17cf1edeb4ef52ae09b5bcd6e5b71d9
Peter Focas <peter.focas@gmail.com>
2023-06-28 07:41:58 +1200

    Added environment variable handling

--- test1.py
+++ test1.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+import PFEnv
+
+PFEnv.print_tw_vars()
+PFEnv.set_tw_vars("test")
+PFEnv.print_tw_vars()
+PFEnv.set_tw_vars("prod")
+PFEnv.print_tw_vars()
+
+client = Client()
+
+message = client.messages.create(
+    to="+6421550915",
+    from_="+14179003746",
+    body="After Upgrade!")
+
+print(message.sid)
+
 print('hi')  # Line 1
 # Line 2
 # Line 3 --
